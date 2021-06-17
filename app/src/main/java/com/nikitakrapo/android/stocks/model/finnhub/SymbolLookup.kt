a8d93444c8e23280b5af00b258634b77ec156b8f
infamoushotdog <n.krapotkin@gmail.com>
2021-06-17 13:14:18 +0500

    Renamed

--- app/src/main/java/com/nikitakrapo/android/stocks/model/finnhub/SymbolLookup.kt
+++ app/src/main/java/com/nikitakrapo/android/stocks/model/finnhub/SymbolLookup.kt
@@ -6,16 +6,15 @@ package com.nikitakrapo.android.stocks.model.finnhub
  * @property result array of search results
  */
 data class SymbolLookup(val count: Int,
-                        val result: List<SingleLookupResponse>){
+                        val result: List<SingleSymbolLookup>){
     /**
      * Represents an element in symbol lookup response
-     * DO NOT USE ANYWHERE FROM symbolLookup!!
      * @property description symbol description
      * @property displaySymbol display symbol name
      * @property symbol unique symbol used to identify this symbol used in /stock/candle endpoint
      * @property type security type
      */
-    data class SingleLookupResponse(
+    data class SingleSymbolLookup(
         val description: String?,
         val displaySymbol: String?,
         val symbol: String?,
